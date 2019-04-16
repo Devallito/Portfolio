87f7fe86cf68d5bf6131911c6a18ed818c7ad1de
abonvalle <alexandre.bonvalle@gmail.com>
2019-04-16 23:05:39 +0200

    welcome et fonctionnement

--- current/main.js
+++ current/main.js
@@ -98,7 +98,7 @@ function writeInDOMRecursively(aList) {
   if (aList.type === 'folder') {
     element.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><title>' + aList.caption + '</title><path fill="#9DA5B4" fill-rule="evenodd" d="M13 4H7V3c0-.66-.31-1-1-1H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1zM6 4H1V3h5v1z"/></svg><span class="treeview_span_elemName" title="' + aList.caption + '">' + aList.caption + '</span>';
   } else if (aList.type === 'file') {
-    element.setAttribute("onclick","openInTab(this.id)");
+    element.setAttribute("onclick", "openInTab(this.id)");
     element.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><title>' + aList.caption + '</title><path fill="#9DA5B4" fill-rule="evenodd" d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"/></svg><span class="treeview_span_elemName" title="' + aList.caption + '">' + aList.caption + '</span>';
   }
   document.getElementById("treeview").appendChild(element);
@@ -185,29 +185,53 @@ function charge_treeview(aListToOrder) {
 }
 
 treeview_list = [{
+  PId: -2,
+  Id: -2,
+  lvl: -2,
+  caption: "Welcome",
+  type: "noshow",
+  path: "Welcome",
+  html: '<div class="Welcome_page_left">\r\n<h1>Développeur Full-Stack</h1>\r\n<h3><a href="">A hackable portfolio for the 21<sup>st</sup> Century</a></h3>\r\n<p>naviguer comme ide</p>\r\n</div>\r\n\r\n<div class="Welcome_page_right">\r\n<div class="Welcome_page_box"><span class="Welcome_page_box_icon">Lire </span>README.md</div>\r\n<div class="Welcome_page_box"><span class="Welcome_page_box_icon">Ouvrir </span>les fichiers</div>\r\n<div class="Welcome_page_box"><span class="Welcome_page_box_icon">Open </span>c</div>\r\n<div class="Welcome_page_box"><span class="Welcome_page_box_icon">Open </span>d</div>\r\n</div>'
+}, {
   PId: -1,
   Id: 1,
   lvl: 0,
   caption: "root",
-  type: "folder"
+  type: "folder",
+  path: "",
+  html: ""
 }, {
   PId: 1,
   Id: 2,
   lvl: 1,
-  caption: "index.html",
-  type: "file"
+  caption: "README.md",
+  type: "file",
+  path: "README.md",
+  html: ""
 }, {
   PId: 1,
   Id: 3,
   lvl: 1,
-  caption: "css",
-  type: "folder"
+  caption: "index.html",
+  type: "file",
+  path: "index.html",
+  html: ""
 }, {
-  PId: 3,
+  PId: 1,
   Id: 4,
+  lvl: 1,
+  caption: "css",
+  type: "folder",
+  path: "",
+  html: ""
+}, {
+  PId: 4,
+  Id: 5,
   lvl: 2,
   caption: "style.css",
-  type: "file"
+  type: "file",
+  path: "css\style.css",
+  html: ""
 }];
 
 charge_treeview(treeview_list);
@@ -232,32 +256,60 @@ document.querySelectorAll(".title_barre_outil").forEach(function(element) {
 
 });
 
+function getPath(aCaption) {
+  return treeview_list.find(function(anElement) {
+    return aCaption === anElement.caption;
+  }).path;
+}
+
 function openInTab(idNodeToOpen) {
   let nameFile = idNodeToOpen;
+  document.getElementById("namePage_footer").innerText = getPath(nameFile); //set path
   document.getElementsByName("navtabs").forEach(function(aTab) {
-    console.log(aTab);
     aTab.classList.remove("barre_nav_tabs_currOpen");
   });
-  let currTab = document.getElementById(nameFile+"_tab");
+  let currTab = document.getElementById(nameFile + "_tab");
   if (currTab) {
     currTab.classList.add("barre_nav_tabs_currOpen");
-    showTab(currTab);
+    showTab(nameFile);
   } else {
     let tab = document.createElement("div");
-    tab.setAttribute("id", nameFile+"_tab");
+    tab.setAttribute("id", nameFile + "_tab");
     tab.setAttribute("name", "navtabs");
-    tab.setAttribute("onclick", "openInTab('"+nameFile+"')");
+    tab.setAttribute("onclick", "openInTab('" + nameFile + "')");
     tab.setAttribute("class", "barre_nav_tabs barre_nav_tabs_currOpen");
     tab.innerText = nameFile;
     document.getElementById("barre_nav").appendChild(tab);
-    createTab(aNode);
+    createTab(nameFile);
   }
 }
 
-function createTab(aNode) {
+function createTab(anId) {
+  let result = treeview_list.find(function(anItem) {
+    return anItem.caption === anId;
+  });
+
+  let aPage = document.createElement("div");
+  aPage.setAttribute("id", result.caption + "_page");
+  aPage.setAttribute("name", "pageTab");
+  aPage.innerHTML = result.html;
+  document.getElementById("pages").appendChild(aPage);
+  showTab(anId);
 
 }
 
-function showTab(aNode) {
-
+function showTab(anId) {
+  document.getElementsByName("pageTab").forEach(function(aPage) {
+    if (aPage.id === anId + "_page") {
+      aPage.classList.remove("hide");
+    } else {
+      aPage.classList.add("hide");
+    }
+  });
 }
+
+openInTab("Welcome"); //init with welcome page
+
+document.getElementById("panel_left").addEventListener("hover", function() {
+  //show hidingpanelbutton
+})
